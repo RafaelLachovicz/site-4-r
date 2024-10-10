ab3bd3e8f861264a712531977f06fddfefd45cba
RafaelLachovicz <160141452+RafaelLachovicz@users.noreply.github.com>
2024-10-10 11:35:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é um algoritimo?',
+    'Um algoritmo nada mais é que um texto contendo comandos (instruções) que devem ser executados numa determinada ordem.'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual é o jogo mais vendido do mundo?',
+    'O jogo eletrônico mais vendido até o momento é Minecraft'
+)
+
+criaCartao(
+    'História',
+    'Em que ano o brasil se tornou independente?',
+    'Em 7 de setembro de 1822, o Brasil tornou-se independente de Portugal'
+)
+
+criaCartao(
+    'Ciẽncias',
+    'Quais os tipos de Ciẽncias?',
+    'O Conselho Nacional de Desenvolvimento Científico e Tecnológico (CNPq) classifica os saberes em oito áreas: Ciências Exatas e da Terra; Ciências Biológicas; Engenharias; Ciências da Saúde; Ciências Agrárias; Ciências Sociais; Ciências Humanas; Linguística, Letras e Artes.'
+)
